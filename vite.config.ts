aaa81068e8f2b947a81f21f74622866b3d534dc1
omura risa <risa@omuranoMacBook-Air.local>
2025-05-20 14:57:59 +0900

    may20

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/4pro2025/', // 
+  build: {
+    outDir: 'docs', // 出力先を 'docs' に変更
+  },
 })
